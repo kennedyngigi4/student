a69ae10f4815cb4cfab5743f1d729e8cc2408552
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-02-19 21:31:33 +0300

    Meeting resolve params

--- app/student/(student)/(routes)/meetings/[meetingId]/page.tsx
+++ app/student/(student)/(routes)/meetings/[meetingId]/page.tsx
@@ -2,15 +2,16 @@
 
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useSession } from 'next-auth/react';
-import React, { useState } from 'react';
+import React, { useState, use } from 'react';
 import MeetingSetup from './_components/meeting-setup';
 import MeetingRoom from './_components/meeting-room';
 import { useGetCallById } from '@/hooks/useGetCallById';
 import Loader from '@/components/Loader';
 
+type Params = Promise<{ meetingId: string  }>;
 
-const MeetingPage = ({ params }: { params: { meetingId: string }}) => {
-    const resolvedParams = React.use(params); 
+const MeetingPage = (props: { params: Params }) => {
+    const resolvedParams = use(props.params); 
     const { status } = useSession();
     const [isSetupComplete, setIsSetupComplete ] = useState(false);
     const { call, isCallLoading } = useGetCallById(resolvedParams?.meetingId);
