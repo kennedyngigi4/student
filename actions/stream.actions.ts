606f35230f7534d4400fdbe65645b6b128e817ec
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-02-19 21:42:01 +0300

    Stream actions update

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -18,7 +18,7 @@ export const tokenProvider = async () => {
     const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
     const issued = Math.floor(Date.now() / 1000) - 60;
 
-    const token = client.createToken(session?.user?.id, exp, issued);
+    const token = client.generateUserToken(session?.user?.id, exp, issued);
     return token;
 
 }
