18de806151dfa852207a8b89e07f5655534a35b4
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-02-25 20:41:06 +0300

    Privacy Policy and terms content added

--- app/(guest)/(routes)/privacy-policy/page.tsx
+++ app/(guest)/(routes)/privacy-policy/page.tsx
@@ -6,8 +6,225 @@ const PrivacyPolicyPage = () => {
         <h1 className="md:text-2xl text-lg font-semibold font-poppins py-10 text-center">ISKY TECH PRIVACY POLICY</h1>
 
         <div className="">
+          <h6 className="font-semibold">Effective Date</h6>
+          <p>
+          Welcome to iSkyTech! Your privacy is important to us, and this Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our website and services. Please read this
+          policy carefully. By accessing or using our website, you agree to the terms outlined in this Privacy
+          Policy.
+          </p>
+        </div>
+
+        <div className="pt-8 py-14">
+          <div className="flex flex-row">
+            <h6 className="font-semibold text-lg">1.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Information We Collect</h6>
+              <p>We may collect the following types of information:</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">1.1</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Personal Information</h6>
+              <p>&bull; Name, email address, phone number, and other contact details when you register, subscribe, or
+              communicate with us.</p>
+              <p>&bull; Payment details when making purchases or subscriptions.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">1.2</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Non-Personal Information</h6>
+              <p>&bull; Browser type, operating system, and IP address for technical purposes and analytics.</p>
+              <p>&bull; Website usage data such as pages visited, time spent, and interactions.</p>
+            </div>
+          </div>
+
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">1.3</h6>
+            <div className="flex flex-col ps-4">
+            <h6 className="font-semibold">Sensitive Information</h6>
+            <p>&bull; If you are a child below 18 years, we may collect additional information with parental consent, such as
+              guardian contact details and proof of consent.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">2.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">How we use your information.</h6>
+              <p>We use the collected information to:</p>
+              <p>&bull; Provide, maintain, and improve our services.</p>
+              <p>&bull; Personalize user experiences.</p>
+              <p>&bull; Process payments and manage accounts.</p>
+              <p>&bull; Communicate updates, marketing or promotional offers(with opt-out options)</p>
+              <p>&bull; Ensure legal and regulatory compliance.</p>
+              <p>&bull; Monitor and prevent fraud and unauthorized activities.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">3.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Sharing Your Information </h6>
+              <p>We do not sell or rent your information. However, we may share your information in the following circumstances:</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">3.1</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">With Third-Party Service Providers</h6>
+              <p>&bull; To facilitate services like payment processing, email delivery, and analytics.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">3.2</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">For Legal and Compliance Reasons</h6>
+              <p>&bull; To comply with applicable laws, regulations, or legal processes. </p>
+              <p>&bull; To protect the rights, property, or safety of iSkyTech, users, or others.  </p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">3.3</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Business Transfers </h6>
+              <p>&bull; In the event of a merger, acquisition, or sale of assets, your information may be transferred to the new entity.  </p>
+            </div>
+          </div>
+
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">4.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Data Retention </h6>
+              <p>We retain personal information only as long as necessary to fulfill the purposes outlined in this policy or as required by law. Upon request, we will delete or anonymize your personal data unless required otherwise by legal obligations.</p>
+            </div>
+          </div>
+
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">5.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Cookies and Tracking Technologies </h6>
+              <p>Our website uses cookies to enhance your experience. Cookies help us: </p>
+              <p>&bull; Understand user preferences.</p>
+              <p>&bull; Monitor website performance.</p>
+              <p>&bull; Deliver personalized content and advertisements. </p>
+              <p>You can manage or disable cookies through your browser settings. However, some website features may not function properly without cookies.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">6.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Data Security</h6>
+              <p>We implement industry-standard measures to protect your information, including:</p>
+              <p>&bull; Encryption of sensitive data.</p>
+              <p>&bull; Regular security audits and updates.</p>
+              <p>&bull; Restricted access to personal data.  </p>
+              <p>While we strive to protect your data, no method of transmission over the internet or electronic storage is 100% secure. </p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Your Rights</h6>
+              
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.1</h6>
+            <div className="flex flex-col ps-4">
+              <p className="">To be informed of the use to which your personal data is to be put;</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.2</h6>
+            <div className="flex flex-col ps-4">
+              <p className="">To access your personal data in our custody; </p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.3</h6>
+            <div className="flex flex-col ps-4">
+              <p className="">To object to the processing of all or part of your personal data;</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.4</h6>
+            <div className="flex flex-col ps-4">
+              <p className="">To correction of false or misleading data; and </p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">7.5</h6>
+            <div className="flex flex-col ps-4">
+              <p className="">To deletion of false or misleading data about yourself. </p>
+              <p>To exercise these rights, contact us at <a href="/">developer@iskytech.net</a> . We may require proof of identity before processing your request. </p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">8</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Third-Party Links</h6>
+              <p>Our website may contain links to third-party websites. We are not responsible for the privacy practices or content of these websites. We encourage you to review their privacy policies. </p>
+            </div>
+          </div>
+
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">9</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Children's Privacy</h6>
+              <p>If you are under the age of 18, parental or guardian consent is required before using our services. We do not knowingly collect personal data from children without appropriate consent. Parents can contact us to review or delete their child’s information.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">10</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">International Data Transfers</h6>
+              <p>If you are accessing our website from outside Kenya, your data may be transferred to and processed in Kenya or other countries. By using our services, you consent to such transfers. </p>
+            </div>
+          </div>
+
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">11</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Changes to This Privacy Policy</h6>
+              <p>We may update this Privacy Policy from time to time. The "Effective Date" at the top of this page indicates the latest revision. We encourage you to review this page regularly for updates.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row pt-3">
+            <h6 className="font-semibold text-lg">12</h6>
+            <div className="flex flex-col ps-4">
+              <h6 className="font-semibold">Contact Us </h6>
+              <p>For questions or concerns about this Privacy Policy or data handling practices, please contact us: <br />
+              iSkyTech Email: <a href="/">developer@iskytech.net</a>  
+              </p>
+            </div>
+          </div>
+
 
         </div>
+
+
     </section>
   )
 }
--- app/(guest)/(routes)/terms-conditions/page.tsx
+++ app/(guest)/(routes)/terms-conditions/page.tsx
@@ -5,11 +5,253 @@ const TermsConditionsPage = () => {
       <section className="flex flex-col md:px-20 px-5 w-full">
           <h1 className="md:text-2xl text-lg font-semibold font-poppins py-10 text-center">ISKY TECH TERMS AND CONDITIONS OF USE</h1>
 
-          <div className="">
-            <h6></h6>
-            <p>
+          <div className="flex flex-row">
+            <h1 className="font-semibold text-lg">1. Introduction</h1>
+          </div>
+
+          <div className="flex flex-col py-10">
+            <div className="flex flex-row">
+              <h6>1.1</h6>
+              <p className="ps-4"> These Terms and Conditions ("Terms") govern your use of our platform, iSkyTech, including all content, functionality, and services offered through the website (the "Site"). </p>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>1.2</h6>
+              <p className="ps-4">By accessing or using the Site, you confirm that you have read, understood, and agreed to these Terms. If you do not agree to these Terms, you must not use the Site. </p>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>1.3</h6>
+              <p className="ps-4">If you are under the age of 18, you must obtain parental or guardian consent before using the Site. Parents or guardians must supervise children’s activities on the Site.</p>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>1.4</h6>
+              <p className="ps-4">These Terms incorporate our Privacy Policy, Cookie Policy, and any additional guidelines or rules applicable to specific services provided by iSkyTech. </p>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>2.</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Ownership and Operator Information</p>
+                <p className="">The Site is operated by iSkyTech (“Company”). Our registered office is at:</p>
+              </div>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>3.</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Subscription Policy</p>
+                <p className="">&bull; <strong>Plans:</strong> Users can purchase individual courses. Subscription plans for 3, 6, or 12 months may be introduced in the future. </p>
+                <p className="">&bull; <strong>Renewals:</strong> Subscriptions, when implemented, will require manual renewal. </p>
+                <p className="">&bull; <strong>Cancellations and Refunds:</strong> No refunds are offered for unused portions of a course or subscription.</p>
+                <p className="">&bull; <strong>Trial Period:</strong> Currently, no trial period is available.</p>
+                <p className="">&bull; <strong>External Links and Third-Party Tools:</strong> The platform may include links to third-party websites or tools for enhanced learning. iSkyTech does not guarantee the safety or accuracy of third-party content and is not liable for issues arising from its use. </p>
+              </div>
+            </div>
+
+
+            <div className="flex flex-row pt-3">
+              <h6>4.</h6>
+              <p className="ps-4 font-semibold">Intellectual Property Rights </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>4.1</h6>
+              <p className="ps-4">All intellectual property rights, including course materials, copyrights, trademarks, and patents related to the Site and its content, are owned by or licensed to iSkyTech unless explicitly stated otherwise.</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>4.2</h6>
+              <p className="ps-4">You may not copy, reproduce, distribute, or create derivative works from the Site’s content without prior written consent from iSkyTech.</p>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>5.</h6>
+              <p className="ps-4 font-semibold">Permitted Use </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>5.1</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Users may:</p>
+                <p className="">&bull; Access and view the Site’s content for personal, non-commercial use. </p>
+                <p className="">&bull; Download or print materials explicitly provided for download.</p>
+                <p className="">&bull; Purchases courses available on the Site </p>
+              </div>
+            </div>
+
+            <div className="flex flex-row pt-3">
+              <h6>5.2</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Users must not:</p>
+                <p className="">&bull; Use the Site in any way that violates applicable laws or regulations. </p>
+                <p className="">&bull; Reverse-engineer, decompile, or disassemble any software or functionality provided on the Site. </p>
+                <p className="">&bull; Circumvent security measures implemented to protect the Site. </p>
+                <p className="">&bull; Reproduce, distribute, or modify platform content without prior authorization.</p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>5.3</h6>
+              <p className="ps-4">Users are responsible for ensuring their use complies with these Terms. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>6.</h6>
+              <p className="ps-4 font-semibold">User Accounts </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>6.1</h6>
+              <div className="flex flex-col ps-4">
+                <p className="">To access certain features, users may need to create an account. By creating an account, you agree to: </p>
+                <p className="">&bull; Provide accurate and complete information. </p>
+                <p className="">&bull; Maintain the confidentiality of your login credentials.  </p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>6.2</h6>
+              <p className="ps-4">Users shall provide their Full names, email, phone numbers, age and grade (level) during the registration of the account. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>6.3</h6>
+              <p className="ps-4">iSkyTech reserves the right to suspend or terminate accounts that violate these Terms or engage in suspicious activities.</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>6.4</h6>
+              <p className="ps-4">Users can terminate their account and request data deletion by contacting developer@iskytech.net. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>7.</h6>
+              <p className="ps-4 font-semibold">User Content and Conduct </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>7.1</h6>
+              <p className="ps-4">Users may contribute content (e.g., comments, submissions) to the Site. By doing so, you grant iSkyTech a non-exclusive, royalty-free license to use, reproduce, and distribute your content. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>7.2</h6>
+              <div className="flex flex-col ps-4">
+                <p className="">Users must ensure their content: </p>
+                <p className="">&bull; Does not infringe on intellectual property rights.</p>
+                <p className="">&bull; Is not defamatory, obscene, or harmful.</p>
+                <p className="">&bull; Complies with all applicable laws and regulations.</p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>7.3</h6>
+              <p className="ps-4"> iSkyTech reserves the right to review, edit, or remove user content that violates these Terms. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>8.</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Payment Policy. </p>
+                <p className="">&bull; Accepted Methods: TBD</p>
+                <p className="">&bull; Security: Payments are processed through authorized third-party APIs to ensure data protection. </p>
+                <p className="">&bull; Additional Charges: Users are responsible for transaction fees imposed by financial institutions. </p>
+                <p className="">&bull; Receipts: Invoices are emailed to users upon payment. </p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>9.</h6>
+              <p className="ps-4 font-semibold">Data Protection and Privacy</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>9.1</h6>
+              <p className="ps-4">iSkyTech is committed to protecting your personal data in compliance with Kenyan Data Protection Laws and the General Data Protection Regulation (GDPR). </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>9.2</h6>
+              <p className="ps-4">For detailed information on how we handle your data, please refer to our Privacy Policy. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>10.</h6>
+              <p className="ps-4 font-semibold">Limitation of Liability </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>10.1</h6>
+              <p className="ps-4">The Site is provided on an "as-is" and "as-available" basis. iSkyTech does not warrant the accuracy, completeness, or reliability of the Site’s content or functionality. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>10.2</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">To the extent permitted by law, iSkyTech shall not be liable for: </p>
+                <p className="">&bull; Direct, indirect, or consequential damages arising from your use of the Site. </p>
+                <p className="">&bull; Loss of data, profits, or business opportunities. </p>
                 
-            </p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>11.</h6>
+              <p className="ps-4 font-semibold">Severability</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>11.1</h6>
+              <p className="ps-4">If a provision of these terms and conditions is determined by any court or other competent authority to be unlawful and/or unenforceable, the other provisions will continue in effect. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>11.2</h6>
+              <p className="ps-4">If any unlawful and/or unenforceable provision of these terms and conditions would be lawful or enforceable if part of it were deleted, that part will be deemed to be deleted, and the rest of the provision will continue in effect.</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>12.</h6>
+              <p className="ps-4 font-semibold">Amendments</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>12.1</h6>
+              <p className="ps-4">iSkyTech reserves the right to amend these Terms at any time. The revised version will take effect immediately upon being published on the iSkyTech website. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>12.2</h6>
+              <p className="ps-4">Continued use of the Site following changes indicates acceptance of the revised Terms.</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>12.3</h6>
+              <p className="ps-4">Users will be notified of significant changes via email or platform announcements.</p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>13.</h6>
+              <p className="ps-4 font-semibold">Breaches of Terms and Conditions </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>13.1</h6>
+              <div className="flex flex-col ps-4">
+                <p className="">If a user violates these Terms and Conditions or if iSkyTech reasonably suspects a violation, we may take one or more of the following actions: </p>
+                <p className="">&bull; Issue formal warnings to the user; </p>
+                <p className="">&bull; Temporarily suspend the user’s access to the platform.</p>
+                <p className="">&bull; Permanently revoke the user’s access to the platform.</p>
+                <p className="">&bull; Restrict access to the platform by blocking specific IP addresses.</p>
+                <p className="">&bull; Notify the user’s internet service provider to block access.</p>
+                <p className="">&bull; Suspend or delete the user’s account; and </p>
+                <p className="">&bull; Institute legal proceedings against the user for breach of contract or other relevant legal claims. </p>
+                <p className="">&bull; Users whose access has been restricted must not attempt to bypass such measures, including creating new accounts to circumvent suspensions or prohibitions. </p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>14.</h6>
+              <p className="ps-4 font-semibold">Governing Law and Dispute Resolution </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>14.1</h6>
+              <p className="ps-4">These Terms shall be governed by and construed in accordance with the laws of Kenya. </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>14.2</h6>
+              <p className="ps-4">Any disputes arising under these Terms shall be subject to the exclusive jurisdiction of the courts of the Republic of Kenya </p>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>15.</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Indemnity</p>
+                <p className="">Users agree to indemnify and hold harmless iSkyTech, its affiliates, and staff from any claims, damages, or expenses arising from their use of the platform. </p>
+              </div>
+            </div>
+            <div className="flex flex-row pt-3">
+              <h6>16.</h6>
+              <div className="flex flex-col ps-4">
+                <p className="font-semibold">Contact Information </p>
+                <p className="">For questions or concerns about these Terms, contact us at: </p>
+                <p className="">iSkyTech Email: developer@iskytech.net</p>
+                <p className="">By using iSkyTech, you acknowledge that you have read, understood, and agree to be bound by these Terms and Conditions. </p>
+              </div>
+            </div>
+
+
           </div>
       </section>
   )
